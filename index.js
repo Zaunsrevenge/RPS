ceae1d83ad2ff822f618f578e023b27531eecad0
Zaunsrevenge <123345261+Zaunsrevenge@users.noreply.github.com>
2023-08-22 17:57:04 +0100

    scoreboard added

--- index.js
+++ index.js
@@ -1,7 +1,10 @@
 const choices = ["rock", "paper", "scissors"]
-
+const winners = []
 function game() {
-    playround();
+    for (let i = 0; i <= 4; i++) {
+        playround();
+    }
+    logWins();
 }
 
 // play the game 
@@ -11,9 +14,15 @@ function game() {
 function playround() {
     const playerSelection = playerChoice();
     const computerSelection = computerChoice();
+    const winner = checkWinner(playerSelection, computerSelection);
+    winners.push(winner)
+    console.log(playerSelection)
+    console.log(computerSelection)
+    console.log(winner)
 
 }
 
+//PLAYER CHOICE CODE//
 function playerChoice() {
     let input = prompt('Type Rock, Paper, or Scissors');
     while (input == null) {
@@ -29,12 +38,12 @@ function playerChoice() {
         input = input.toLowerCase();
         check = validateInput(input);
     }
-
+    return input;
 }
 
 
 
-
+//COMPUTER CHOICE CODE//
 function computerChoice() {
     //Get input from computer
     return choices[Math.floor(Math.random() * choices.length)]
@@ -48,6 +57,31 @@ function validateInput(choice) {
     else {
         return false;
     }
+}
+//RESULTS OF ROUND CODE//
+function checkWinner(choiceP, choiceC) {
+    if (choiceP === choiceC) {
+        return 'Tie';
+    } else if (
+        (choiceP === 'rock' && choiceC === 'scissors') ||
+        (choiceP === 'paper' && choiceC === 'rock') ||
+        (choiceP === 'scissors' && choiceC === 'paper')) {
+        return 'Player'
+    } else {
+        return 'Computer'
+    }
+}
+
+//Scoreboard Code//
+function logWins() {
+    let playerWins = winners.filter((item) => item == 'Player').length;
+    let computerWins = winners.filter((item) => item == 'Computer').length;
+    let ties = winners.filter((item) => item == 'Tie').length;
+    console.log('Results:');
+    console.log('Player Wins', playerWins);
+    console.log('Computer Wins', computerWins);
+    console.log('ties', ties);
+
 }
 
 game();
